81f8d954cbd397a5c8cdd33e8db0b8541b9fcbe5
andela-tolotin <temitope.olotin@andela.com>
2016-04-24 23:35:18 +0100

    [Feature #118255043] Define the structure of the video view

---
+++ resources/views/main/pages/view_video_details.blade.php
@@ -0,0 +1,11 @@
+<h3 align="center">What Generators in PHP really are </h3>
+<div class="container v-center">
+   <div class="row">
+      <div class="col-lg-12">
+            <div class="video_wrapper">
+            </div>
+            <div class="video_details">
+            </div>
+      </div>
+  </div>
+</div>
