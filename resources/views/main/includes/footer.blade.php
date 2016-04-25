c0f4c04ab139e12ed206c6d24e4171eec889d03b
andela-tolotin <temitope.olotin@andela.com>
2016-04-25 23:00:03 +0100

    [Feature #118336839] Create signup form

--- resources/views/main/includes/footer.blade.php
+++ resources/views/main/includes/footer.blade.php
@@ -13,11 +13,14 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h2 class="text-center"><img src="//placehold.it/110" class="img-circle"><br>Login</h2>
+                <h2 class="text-center">SIGN UP</h2>
             </div>
             <div class="modal-body row">
                 <h6 class="text-center">COMPLETE THESE FIELDS TO SIGN UP</h6>
                 <form class="col-md-10 col-md-offset-1 col-xs-12 col-xs-offset-0">
+                <div class="form-group">
+                        <input type="text" class="form-control input-lg" placeholder="Username">
+                    </div>
                     <div class="form-group">
                         <input type="text" class="form-control input-lg" placeholder="Email">
                     </div>
@@ -25,8 +28,8 @@
                         <input type="password" class="form-control input-lg" placeholder="Password">
                     </div>
                     <div class="form-group">
-                        <button class="btn btn-danger btn-lg btn-block">Sign In</button>
-                        <span class="pull-right"><a href="signup">Register</a></span><span><a href="#">Need help?</a></span>
+                        <button class="btn btn-danger btn-lg btn-block">Sign up</button>
+                        <span class="pull-right"><a href="/login">Log In</a></span>
                     </div>
                 </form>
             </div>
