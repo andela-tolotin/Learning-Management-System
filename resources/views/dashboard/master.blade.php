72f9ac0d2da30276f7a5dd00f69e16ab4a1a6962
andela-tolotin <temitope.olotin@andela.com>
2016-04-28 00:38:28 +0100

    [Feature #118511747] Create dashboard master page

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -1,7 +1,15 @@
-@include('dashboard.includes.links_and_metadata')
-<div class="container">
-    <div class="content">
-    @include('dashboard.includes.nav_bar')
-        @yield('content')
-    </div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   @include('dashboard.includes.links_and_metadata')
+<title>@yield('title')</title>
+</head>
+<body>
+@include('dashboard.includes.nav_bar')
+<main>
+@include('dashboard.includes.top_nav')
+  @yield('content')
+</main>
+   @include('dashboard.includes.footer')
+</body>
+</html>
