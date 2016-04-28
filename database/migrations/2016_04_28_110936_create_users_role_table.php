6d1aa1a897a4dd7b6ce2274b4f9eb1dc5ecfb7e0
andela-tolotin <temitope.olotin@andela.com>
2016-04-28 12:54:23 +0100

    [Feature #118552765] Add user role migration

---
+++ database/migrations/2016_04_28_110936_create_users_role_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->integer('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('roles');
+    }
+}
