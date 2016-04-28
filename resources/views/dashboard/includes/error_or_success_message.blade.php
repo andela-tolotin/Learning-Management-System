0bcf412ceffbe4f623f3289f45b6fe87330b77b2
andela-tolotin <temitope.olotin@andela.com>
2016-04-29 00:20:01 +0100

    [Feature #118552765] Re-arrange message display order

--- resources/views/dashboard/includes/error_or_success_message.blade.php
+++ resources/views/dashboard/includes/error_or_success_message.blade.php
@@ -1,8 +1,18 @@
 <div class="row">
  <div class="input-field col s8 offset-m3">
-   @if (count($errors) > 0)
-   <!-- Form Error List -->
+   @if (session('status') == 'Category already exists!')
    <div class="card-panel deep-orange accent-4 danger">
+    <strong> {{ session('status') }}</strong>
+  </div>
+  @endif
+  @if (session('status') == 'Sucessfully created!') 
+  <div class="card-panel teal darken-4 success">
+    <strong> {{ session('status') }}</strong>
+  </div>
+  @endif
+  @if (count($errors) > 0)
+  <!-- Form Error List -->
+  <div class="card-panel deep-orange accent-4 danger">
     <strong>Whoops! Something went wrong!</strong>
     <br>
     <ul>
@@ -10,14 +20,6 @@
       <li>{{ $error }}</li>
       @endforeach
     </ul>
-  </div>
-  @elseif (session('status') == 'Sucessfully created!') 
-  <div class="card-panel teal darken-4 success">
-    <strong> {{ session('status') }}</strong>
-  </div>
-  @elseif (session('status') == 'Category already exists!')
-  <div class="card-panel deep-orange accent-4 danger">
-    <strong> {{ session('status') }}</strong>
   </div>
   @endif
 </div>
