eff1b2314f935194a29058a645963474bba0e6c4
andela-tolotin <temitope.olotin@andela.com>
2016-04-29 17:42:26 +0100

    [Feature #118552765] Add more scopes

--- app/Category.php
+++ app/Category.php
@@ -10,10 +10,33 @@ class Category extends Model
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
-    protected $fillable  = ['name', 'description'];
+
+    protected $fillable  = ['name', 'description', 'user_id'];
 
     public function video()
     {
         return $this->hasOne('App\Video');
+    }
+
+    public function scopeGetCategoriesByUserId($query, $id)
+    {
+        return $query->where('user_id', '=', $id);
+    }
+
+    public function scopeAllTrashedCategories($query, $id)
+    {
+        return $query
+        ->where('user_id', '=', $id)
+        ->onlyTrashed();
+    }
+
+    public function scopeGetCategoryById($query, $id)
+    {
+        return $query->where('id', '=', $id);
+    }
+
+    public function scopeSetCategoryStatus($query, $id)
+    {
+        return $query->find($id);
     }
 }
