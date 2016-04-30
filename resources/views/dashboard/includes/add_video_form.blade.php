eb523a82588897f94aa3688992a27f3b38c4bc3f
andela-tolotin <temitope.olotin@andela.com>
2016-04-30 10:18:28 +0100

    [Feature #118690641] Add form fields names and form actions

--- resources/views/dashboard/includes/add_video_form.blade.php
+++ resources/views/dashboard/includes/add_video_form.blade.php
@@ -1,9 +1,10 @@
 <div class="card-panel hoverable">
-  <form class="col s12" method="POST">
+ @include('dashboard.includes.error_or_success_message')
+  <form class="col s12" method="POST" action="/dashboard/video/create">
    <div class="row">
     <div class="input-field col s8 offset-m3">
       <select>
-      <option value="" selected>Video Category</option>
+      <option value="" selected name="category" id="category">Video Category</option>
         <option value="1">Option 1</option>
         <option value="2">Option 2</option>
         <option value="3">Option 3</option>
@@ -13,13 +14,19 @@
   </div>
   <div class="row">
     <div class="input-field col s8 offset-m3">
-      <input id="title" type="text" class="validate">
+      <input id="title" type="text" class="validate" name="title" value="{{ old('title')}}">
       <label for="name">Title</label>
     </div>
   </div>
   <div class="row">
     <div class="input-field col s8 offset-m3">
-      <textarea id="description" class="materialize-textarea"></textarea>
+      <input id="url" type="text" class="validate" name ="url" value="{{ old('title')}}">
+      <label for="url">Url</label>
+    </div>
+  </div>
+  <div class="row">
+    <div class="input-field col s8 offset-m3">
+      <textarea id="description" class="materialize-textarea" name="description">{{ old('description')}}</textarea>
       <label for="description">Description</label>
     </div>
   </div>
