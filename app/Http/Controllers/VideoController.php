46e3e8631c20652c13a28b9e2bdc140bc8a60c5e
andela-tolotin <temitope.olotin@andela.com>
2016-04-30 23:46:01 +0100

    [Feature #118695887] Implement video controller

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -23,10 +23,13 @@ class VideoController extends Controller
 
     public function store(VideoRequest $request)
     {
+        $user_id = 10;
+
         $category = Video::create([
             'title'        => $request->input('title'),
             'url'          => $request->input('url'),
             'category_id'  => $request->input('category'),
+            'user_id'      => $user_id,
             'description'  => $request->input('description'),
             ]);
 
@@ -34,26 +37,96 @@ class VideoController extends Controller
             return redirect('/dashboard/video/add')->with(
                 'status', 
                 'Sucessfully created!'
-                );
+            );
         } 
 
         return redirect('/dashboard/video/add')->with(
             'status', 
             'Oops! Something went wrong!'
-            );
+        );
     }
 
     public function viewAllVideos()
     {
-        $user_id = 1;
+        $user_id = 10;
 
-        $videos = Video::with('category')->getVideosByUserId($user_id)
+        $videos = Video::with('category')
+        ->getVideosByUserId($user_id)
         ->paginate(10);
 
-        $pendingVideos = Video::with('category')->allTrashedVideos($user_id)
+        $pendingVideos = Video::with('category')
+        ->allTrashedVideos($user_id)
         ->paginate(10);
 
         return view('dashboard.pages.list_all_videos', compact('videos', 'pendingVideos'));
 
+    }
+
+    public function getVideo($id)
+    {
+        $video = Video::getVideoById($id)
+        ->get()
+        ->first();
+
+        $categories = Category::all();
+
+        if (is_null($video)) {
+            return redirect('/dashboard/video/add')->with(
+                'status', 
+                'Oops! Video does not exist!'
+            );
+        }
+
+        return view('dashboard.pages.view_video', compact('categories', 'video'));
+    }
+
+    public function updateVideo(VideoRequest $request, $id)
+    {
+        $video = Video::getVideoById($id)
+        ->update([
+            'title'        => $request->input('title'),
+            'url'          => $request->input('url'),
+            'category_id'  => $request->input('category'),
+            'description'  => $request->input('description'),
+        ]);
+
+        if (! is_null($video)) {
+            return redirect('/dashboard/video/view');
+        } 
+
+        return redirect('/dashboard/video/edit'.$id)->with(
+            'status', 
+            'Oops! Something went wrong!'
+        );
+    }
+
+    public function changeVideoStatus(Request $request, $id) {
+        $video = null;
+
+        if ($request->input('status') == 0) {
+            $video = Video::setVideoStatus($id)
+            ->delete();
+        } else {
+            $video = Video::setVideoStatus($id)
+            ->restore();
+        }
+
+        return $this->returnChangeVideoStatus($video);
+        
+    }
+
+    public function returnChangeVideoStatus($classObject)
+    {
+        if (! is_null($classObject)) {
+            return [
+            'statuscode' => 200,
+            'message' => 'Operation Successfully'
+            ];
+        }
+
+        return [
+        'statuscode' => 404,
+        'message' => 'Invalid Video ID!'
+        ];
     }
 }
