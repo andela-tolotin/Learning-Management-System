fec4e63d12d4ce3596a50cea5becac6c57cfa187
andela-tolotin <temitope.olotin@andela.com>
2016-05-01 21:27:31 +0100

    [Feature #118701803] Modify class attritubes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,12 @@
 @include('main.includes.nav_bar')
 @include('main.includes.top_section')
 <section class="container-fluid" id="section2">
-<h3 align="center">Video Categories</h3>
+<h4 align="center">Video Categories</h4>
+<div class="divider"></div>
 @include('main.includes.video_category_slide')
-<h3 align="center">Featured Videos</h3>
+<div class="divider"></div>
+<h4 align="center">Featured Videos</h4>
 @include('main.includes.learning_content')
 </section>
+
 @include('main.includes.footer')
