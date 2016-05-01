5589a5d0bd92992405506685c2da1aea1af5af7f
andela-tolotin <temitope.olotin@andela.com>
2016-05-01 21:24:25 +0100

    [Feature #118701803] Define relationship with user model

--- app/Video.php
+++ app/Video.php
@@ -18,6 +18,11 @@ class Video extends Model
         return $this->belongsTo('App\Category');
     }
 
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
     public function scopeGetVideosByUserId($query, $id)
     {
         return $query 
