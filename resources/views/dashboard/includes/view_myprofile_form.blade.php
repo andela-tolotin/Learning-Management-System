1a7a3ac52b6f985c9f8bc6abf196e5ec380c1afa
andela-tolotin <temitope.olotin@andela.com>
2016-05-02 16:28:00 +0100

    [Feature 118758203] Set the logged in user data in the existing profile form

--- resources/views/dashboard/includes/view_myprofile_form.blade.php
+++ resources/views/dashboard/includes/view_myprofile_form.blade.php
@@ -1,19 +1,20 @@
 <div class="card-panel hoverable">
+ @include('dashboard.includes.error_or_success_message')
   <form class="form" method="POST">
     <div class="row">
      <div class="col s8">
      <div class="input-field col s10">
-        <input id="username" type="text" class="validate">
+        <input id="username" type="text" class="validate" value="{{ Auth::user()->username }}">
         <label for="username">Username</label>
       </div>
 
       <div class="input-field col s10">
-        <input id="email" type="email" class="validate">
+        <input id="email" type="email" class="validate" value="{{ Auth::user()->email }}">
         <label for="email">Email</label>
       </div>
 
       <div class="input-field col s10">
-        <textarea id="description" class="materialize-textarea"></textarea>
+        <textarea id="description" class="materialize-textarea">{{ Auth::user()->profile_bio }}</textarea>
         <label for="description">Biodata</label>
       </div>
 
@@ -25,7 +26,7 @@
    </div>
    <div class="col s4">
      <div class="input-field col s10">
-        <img src="{{ URL::to('/') }}/images/user.jpg" title="myprofile" alt="myprofile" class="img-circle">
+        <img src="{{ Auth::user()->picture_url }}" title="myprofile" alt="myprofile" class="img-circle">
       </div>
       <div class="input-field col s10">
         <button class="btn waves-effect waves-light" type="submit" name="action">Upload
