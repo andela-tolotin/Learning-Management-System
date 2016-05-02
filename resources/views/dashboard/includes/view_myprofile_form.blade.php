47ab60f235b952397391b255e4a02a18efae012c
andela-tolotin <temitope.olotin@andela.com>
2016-05-02 16:46:33 +0100

    [Feature #118758203] Add form action

--- resources/views/dashboard/includes/view_myprofile_form.blade.php
+++ resources/views/dashboard/includes/view_myprofile_form.blade.php
@@ -1,39 +1,39 @@
 <div class="card-panel hoverable">
  @include('dashboard.includes.error_or_success_message')
-  <form class="form" method="POST">
-    <div class="row">
-     <div class="col s8">
+ <form class="form" method="POST" action="/dashboard/profile/update">
+  <div class="row">
+   <div class="col s8">
      <div class="input-field col s10">
-        <input id="username" type="text" class="validate" value="{{ Auth::user()->username }}">
-        <label for="username">Username</label>
-      </div>
+      <input id="username" type="text" class="validate" value="{{ Auth::user()->username }}">
+      <label for="username">Username</label>
+    </div>
 
-      <div class="input-field col s10">
-        <input id="email" type="email" class="validate" value="{{ Auth::user()->email }}">
-        <label for="email">Email</label>
-      </div>
+    <div class="input-field col s10">
+      <input id="email" type="email" class="validate" value="{{ Auth::user()->email }}">
+      <label for="email">Email</label>
+    </div>
 
-      <div class="input-field col s10">
-        <textarea id="description" class="materialize-textarea">{{ Auth::user()->profile_bio }}</textarea>
-        <label for="description">Biodata</label>
-      </div>
+    <div class="input-field col s10">
+      <textarea id="description" class="materialize-textarea" name="profile_bio">{{ Auth::user()->profile_bio }}</textarea>
+      <label for="description">Biodata</label>
+    </div>
 
-      <div class="input-field col s10">
-        <button class="btn waves-effect waves-dark" type="submit" name="action">Update
-         <i class="material-icons right">update</i>
-       </button>
-     </div>
-   </div>
-   <div class="col s4">
-     <div class="input-field col s10">
-        <img src="{{ Auth::user()->picture_url }}" title="myprofile" alt="myprofile" class="img-circle">
-      </div>
-      <div class="input-field col s10">
-        <button class="btn waves-effect waves-light" type="submit" name="action">Upload
-         <i class="material-icons right">offline_pin</i>
-       </button>
-     </div>
+    <div class="input-field col s10">
+      <button class="btn waves-effect waves-dark" type="submit" name="action">Update
+       <i class="material-icons right">update</i>
+     </button>
    </div>
  </div>
+ <div class="col s4">
+   <div class="input-field col s10">
+    <img src="{{ Auth::user()->picture_url }}" title="myprofile" alt="myprofile" class="img-circle">
+  </div>
+  <div class="input-field col s10">
+    <button class="btn waves-effect waves-light" type="submit" name="action">Upload
+     <i class="material-icons right">offline_pin</i>
+   </button>
+ </div>
+</div>
+</div>
 </form>
 </div>
