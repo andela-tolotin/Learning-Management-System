79cf6598a8380fe3e7f378d159a37a59e9ee8b96
andela-tolotin <temitope.olotin@andela.com>
2016-05-02 22:36:23 +0100

    [ Feature #118701803] Write a test that assert that signup is successfully

--- tests/UserSignUpTest.php
+++ tests/UserSignUpTest.php
@@ -10,23 +10,14 @@ class UserSignUpTest extends TestCase
 
     public function testCheckAllFieldsEmpty()
     {
-        $this->WithoutMiddleware();
+        $user = factory('App\User')->create();
 
-        $user = factory('App\User')->create([
-            'username' => 'lytopze', 
-            'email'    => 'lytopze@gmail.com', 
-            'password' => 'tope0852', 
-            'role_id'  => 1, 
-        ]);
-
-        $response = $this->actingAs($user)
-           ->call('POST', 'auth/register', [
-               'username'    => 'lytopze',
-               'email'       => 'lytopze@gmail.com', 
-               'password'    => 'tope0852',
-           ]);
-
-        var_dump($response->getContent());
+           $this->visit('/')
+           ->type('lytopze', 'username')
+           ->type('lytopze@gmail.com', 'email')
+           ->type('tope0852', 'password')
+           ->press('Sign up')
+           ->see('lytopze');
     }
 
 }
